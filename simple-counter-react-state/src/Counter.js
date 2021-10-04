69f38ac15ba6d8f6eeeb1c82767ed326c58c0065
Paul Tobin <tobster111@gmail.com>
2021-10-04 12:34:23 +0100

    Adding state using constructor and this.state

--- simple-counter-react-state/src/Counter.js
+++ simple-counter-react-state/src/Counter.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: 3
+    }
+  }
+
   render() {
+    const {count} = this.state
+    
     return (
       <div className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
           <button>Increment</button>
           <button>Decrement</button>
