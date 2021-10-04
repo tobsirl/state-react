33e11348e661e6a37883d5cb9f6c58b92d3e5d90
Paul Tobin <tobster111@gmail.com>
2021-10-04 14:25:45 +0100

    binding this to the functions

--- simple-counter-react-state/src/Counter.js
+++ simple-counter-react-state/src/Counter.js
@@ -5,8 +5,12 @@ class Counter extends Component {
     super(props)
 
     this.state = {
-      count: 3,
+      count: 0,
     }
+
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+    this.reset = this.reset.bind(this)
   }
 
   increment() {
@@ -28,9 +32,9 @@ class Counter extends Component {
       <div className="Counter">
         <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </div>
     )
