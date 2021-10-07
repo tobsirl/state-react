deb1600cdf3cc78186d0b2cd2d4f6aa7453317cd
Paul Tobin <tobster111@gmail.com>
2021-10-07 12:10:35 +0100

    Using dispatch with type and payload

--- grudges-react-state/src/Application.js
+++ grudges-react-state/src/Application.js
@@ -7,26 +7,29 @@ import NewGrudge from './NewGrudge';
 
 import initialState from './initialState';
 
+const GRUDGE_ADD = 'GRUDGE_ADD'
+const GRUDGE_FORGIVE = 'GRUDGE_FORGIVE'
+
 const reducer = (state, action) => {
   return state
 }
 
 const Application = () => {
-  const [grudges, setGrudges] = useReducer(reducer, initialState);
+  const [grudges, dispatch] = useReducer(reducer, initialState);
 
-  const addGrudge = grudge => {
-    grudge.id = id();
-    grudge.forgiven = false;
-    setGrudges([grudge, ...grudges]);
+  const addGrudge = ({person, reason}) => {
+    dispatch({ type: GRUDGE_ADD, payload: {
+      person, reason, forgiven: false, id: id()
+    } })
   };
 
   const toggleForgiveness = id => {
-    setGrudges(
-      grudges.map(grudge => {
-        if (grudge.id !== id) return grudge;
-        return { ...grudge, forgiven: !grudge.forgiven };
-      })
-    );
+    // setGrudges(
+    //   grudges.map(grudge => {
+    //     if (grudge.id !== id) return grudge;
+    //     return { ...grudge, forgiven: !grudge.forgiven };
+    //   })
+    // );
   };
 
   return (
