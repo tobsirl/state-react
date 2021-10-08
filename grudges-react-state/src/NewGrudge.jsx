d34ebbdc867704b66f867a57c7a39dddf0f464ba
Paul Tobin <tobster111@gmail.com>
2021-10-08 14:20:10 +0100

    Adding useContext to NewGrudge

--- grudges-react-state/src/NewGrudge.jsx
+++ grudges-react-state/src/NewGrudge.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { GrudgeContext } from './GrudgeContext'
 
 const NewGrudge = React.memo(({ onSubmit }) => {
   const [person, setPerson] = useState('')
   const [reason, setReason] = useState('')
+  const { addGrudge } = useContext(GrudgeContext)
 
   const handleChange = (event) => {
     event.preventDefault()
-    onSubmit({ person, reason })
+    addGrudge({ person, reason })
   }
 
   return (
