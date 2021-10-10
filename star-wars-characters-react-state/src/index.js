74c8d16a61978a63ed2dc025ca82e3299f157900
Paul Tobin <tobster111@gmail.com>
2021-10-10 10:39:51 +0100

    Added a fetchReducer

--- star-wars-characters-react-state/src/index.js
+++ star-wars-characters-react-state/src/index.js
@@ -9,6 +9,40 @@ import endpoint from './endpoint'
 
 import './styles.scss'
 
+const initialState = {
+  result: null,
+  loading: true,
+  error: null,
+}
+
+const fetchReducer = (state, action) => {
+  if (action.type === 'LOADING') {
+    return {
+      result: null,
+      loading: true,
+      error: null,
+    }
+  }
+
+  if (action.type === 'RESPONSE_COMPLETE') {
+    return {
+      result: action.payload.response,
+      loading: false,
+      error: null,
+    }
+  }
+
+  if (action.type === 'ERROR') {
+    return {
+      result: null,
+      loading: false,
+      error: action.payload.error,
+    }
+  }
+
+  return state
+}
+
 const Application = () => {
   const [characters, setCharacters] = useState([])
 
