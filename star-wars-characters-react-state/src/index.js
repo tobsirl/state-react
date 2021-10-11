4ce5d38a6cb75a53b70f0ad7e4186c650de522bd
Paul Tobin <tobster111@gmail.com>
2021-10-11 19:21:47 +0100

    Adding the Sideview and CharacterView

--- star-wars-characters-react-state/src/index.js
+++ star-wars-characters-react-state/src/index.js
@@ -2,9 +2,10 @@ import { isFunction } from 'lodash'
 import React, { useReducer, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import CharacterList from './CharacterList'
+import CharacterView from './CharacterView'
 
 import { endpoint } from './endpoint'
 
@@ -99,6 +100,9 @@ const Application = () => {
             Fetch Characters
           </button>
           <CharacterList characters={characters} />
+        </section>
+        <section className="CharacterView">
+          <Route path="/characters/:id" component={CharacterView} />
         </section>
       </main>
     </div>
