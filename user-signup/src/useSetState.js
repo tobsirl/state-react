5acb6624099bbb908f64a62cad92658ebd41ca58
Paul Tobin <tobster111@gmail.com>
2021-10-16 11:17:45 +0100

    Adding useSetState

---
+++ user-signup/src/useSetState.js
@@ -0,0 +1,10 @@
+import React, {useReducer} from 'react'
+
+export const reducerName = (state = , action) => {
+  switch (action.type) {
+    case 'ACTION_TYPE':
+      return 
+    default:
+      return state
+  }
+}
